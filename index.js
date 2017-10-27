3145c15ed7043726a4693aa87bd70a22c7cdeb61
pirushanp <pirushanp@gmail.com>
2017-10-27 15:14:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var obj = { prop: 1 };
+   return Object.assign({}, obj, { prop2: 2 })
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+    var obj = { prop: 1 }
+    return obj
+
+destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', 2)
+}
+function deleteFromObjectByKey(object, key) {
+  var obj = { prop: 1 }
+  var newObj = deleteFromObjectByKey(obj, 'prop')
+  return newObj
+}
